e86333d0d9f0a1787c94966534f6be21d10de70a
brelance <brelance82@gmail.com>
2023-03-21 21:46:54 +0800

    merge ch1-lab to ch4-lab

--- os/src/console.rs
+++ os/src/console.rs
@@ -31,3 +31,37 @@ macro_rules! println {
 }
 
 
+/// error log
+#[macro_export]
+macro_rules! error {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[31m", "[error] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+/// warn log
+#[macro_export]
+macro_rules! warn {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[93m", "[warn] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+/// info log
+#[macro_export]
+macro_rules! info {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[34m", "[info] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+/// trace log
+#[macro_export]
+macro_rules! trace {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[90m", "[trace] ", $fmt, "\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+
+
